796d1107d200c063687b5ff879ac8a7691a54ae2
abdullah Al Omar <abdullahalomar048@gmail.com>
2023-04-09 14:19:24 +0600

    set up complete

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('pages.home');
+    }
+    public function about()
+    {
+        return view('pages.about');
+    }
+    public function shop()
+    {
+        return view('pages.shop');
+    }
+    public function contact()
+    {
+        return view('pages.contact');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,10 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('shop', [PageController::class, 'shop'])->name('shop');
+Route::get('contact', [PageController::class, 'contact'])->name('contact');
